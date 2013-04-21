77fd603bad67421e7752a51d887f57f5235b6ba2
Eric Gunther <eric.c.gunther@gmail.com>
2013-04-20 20:46:31 -0400

    Styling... got up to section 5.4 in the tutorial

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
+gem 'bootstrap-sass', '2.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
 end
 
 # Gems used only for assets and not required
@@ -19,6 +21,9 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,43 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) { "My App" }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+  describe "Home page" do
+	before { visit root_path }
+	subject { page }
 	
-	it "should have the correct title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => 'My App | Home')
-    end
+	it { should have_selector('h1', text: 'My App') }
+	it { should have_selector('title', text: full_title('')) }
+	it { should_not have_selector('title', text: '| Home') }
+	
+  end
+
+  describe "Contact page" do
+	before { visit contact_path }
+	subject { page }
+	
+	it { should have_selector('h1', text: 'Contact') }
+	it { should have_selector('title', text: full_title('Contact')) }
+
   end
   
    describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+	before { visit help_path }
+	subject { page }
+	
+	it { should have_selector('h1', text: 'Help') }
+	it { should have_selector('title', text: full_title('Help')) }
+  
   end
 
    describe "About page" do
-
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
-    end
+	before { visit about_path }
+	subject { page }
+	
+	it { should have_selector('h1', text: 'About') }
+	it { should have_selector('title', text: full_title('About')) }
+	
   end
   
 end
